8ca8acbf43a255cc789c04e627b1d9f70f16d0b8
Maksym Kochut <feelofgoodvibes@gmail.com>
2022-09-05 03:52:55 +0300

    database schema, pages carcas

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
+from flask import request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from objects import *
 
 
-@app.route("/login")
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        return "EH"
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        login = request.form.get("name")
+        password = request.form.get("password")
+
+        if not login or not password:
+            flash("Name and password fields are required!")
+            return redirect("/login")
+
+        user = User.query.filter(User.login==login).first()
+
+        if not user:
+            flash("This user does not exists!")
+            return redirect("/login")
+
+        if not check_password_hash(user.password, password):
+            flash("Wrong password")
+            return redirect("/login")
+
+        login_user(user, remember=True)
+
+        return redirect("/")
 
 
-@app.route("/logout")
+@app.route("/logout", methods=["POST"])
 def logout():
     pass
 
 
 @app.route("/")
+@login_required
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
--- objects.py
+++ objects.py
@@ -3,18 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required
 import os
 
-from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy import Column, ForeignKey, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+import datetime
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
 app.secret_key = "__jf*DJ*39h8FFF11234"
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
 
 DATABASE_PATH = "database.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{basedir}/{DATABASE_PATH}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = Column(db.Integer, primary_key=True, autoincrement=True)
+    notes = relationship("Note")
+    login = Column(db.String)
+    password = Column(db.String)
+
+class Note(db.Model):
+    __tablename__ = "note"
+
+    id = Column(db.Integer, primary_key=True, autoincrement=True)
+    author = Column(db.Integer, ForeignKey("user.id"))
+    title = Column(db.String(100), nullable=True)
+    text = Column(db.Text, nullable=False)
+    created = Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    edited = Column(db.DateTime, nullable=True)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id==user_id)
+
+if __name__ == "__main__":
+    if os.path.exists(DATABASE_PATH):
+        os.remove(DATABASE_PATH)
+        db.create_all()
